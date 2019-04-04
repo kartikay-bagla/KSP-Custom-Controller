975ed2af4a4d3e90071895716e73e7d2bf74fba0
Kartikay Bagla <kartikaybagla@gmail.com>
2019-04-04 19:41:10 +0530

    Updated get_data to use refactored utils

--- get_data.py
+++ get_data.py
@@ -1,47 +1,19 @@
 import krpc
 import time
 import numpy as np
-from utils import process_resources
+from utils import process_resources, get_vessel, get_server, get_streams
 
 running = True
 server = None
 
-while server is None:
-    print("Connecting")
-    try:
-        server = krpc.connect(name ='Controller', address= krpc.DEFAULT_ADDRESS)
-    except ConnectionRefusedError: #error raised whe failing to connect to the server.
-        print("Server offline")
-        time.sleep(2)
+server = get_server()
 
-time.sleep(2)
 print("Started")
 
-vessel = None
-while True:
-    try: 
-        vessel = server.space_center.active_vessel
-        break
+vessel = get_vessel(server)
 
-    except krpc.error.RPCError as e:
-        print("KSP Scene Changed!")
-        time.sleep(1)
-    except ConnectionAbortedError:
-        print("KSP has Disconnected.")
-        running = False #we can now end the program.
-        
-# Set up streams for telemetry
-altitude = server.add_stream(getattr, vessel.flight(), 'mean_altitude')
-apoapsis = server.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
-periapsis = server.add_stream(getattr, vessel.orbit, 'periapsis_altitude')
-stage_2_resources = vessel.resources_in_decouple_stage(stage=2, cumulative=False)
-resources = server.add_stream(getattr, vessel, "resources")
-srb_fuel = server.add_stream(stage_2_resources.amount, 'SolidFuel')
-thrust = server.add_stream(getattr, vessel, "thrust")
-mass = server.add_stream(getattr, vessel, "mass")
+altitude, apoapsis, peapsis, resources, twr = get_streams(vessel)
 
-def twr():
-    return thrust()/mass()
 
 # Pre-launch setup
 # vessel.control.sas = False
